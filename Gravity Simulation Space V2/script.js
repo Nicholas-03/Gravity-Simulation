634b8814172fd534b96888bb918ea967bdf747a6
Nicholas-03 <61433127+Nicholas-03@users.noreply.github.com>
2021-05-12 22:40:45 +0200

    Add files via upload

--- Gravity Simulation Space V2/script.js
+++ Gravity Simulation Space V2/script.js
@@ -15,6 +15,8 @@ var clickCoordinates = {
     y: null,
     xUp: null,
     yUp: null,
+    xMove: null,
+    yMove: null,
 };
 
 var docSize = {
@@ -22,18 +24,85 @@ var docSize = {
     height: document.documentElement.scrollHeight,
 }
 
+var svg;
+var newLine;
+var isMouseDown = false;
+
+// Called by html body on load
 function start() {
     document.getElementById('staticObj').checked = true;
+
+    svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute('id', 'svg');
+    svg.setAttribute('width', window.innerWidth - 100);
+    svg.setAttribute('height', window.innerHeight - 100);
+    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+    document.body.appendChild(svg);
+
     intervalID = setInterval(loop, 25);
 }
 
+// Update function
 function loop() {
     moveObjects();
+
+    svg.setAttribute('width', window.innerWidth - 100);
+    svg.setAttribute('height', window.innerHeight - 100);
+    if (isMouseDown) {
+        newLine.x2.baseVal.valueAsString = clickCoordinates.xMove + 'px';
+        newLine.y2.baseVal.valueAsString = clickCoordinates.yMove + 'px';
+    }
 }
 
+// Move dynamic objects and check collisions
+function moveObjects() {
+    for (let i = dinamicObjects.length - 1; i >= 0; i--) {
+        var obj = dinamicObjects[i];
+        var isObjRemoved = false;
+
+        addGravityAcceleration(obj);
+
+        for (let sObj of staticObjects) {
+            if (checkCollision(sObj, obj)) {
+                document.body.removeChild(obj.id);
+                dinamicObjects.splice(i, 1);
+                isObjRemoved = true;
+                break;
+            }
+        }
+
+        for (let k = dinamicObjects.length - 1; k >= 0; k--) {
+            if (k != i && !isObjRemoved) {
+                var obj2 = dinamicObjects[k];
+                if (checkCollision(obj, obj2)) {
+                    obj2.radius = Math.sqrt((getObjArea(obj) + getObjArea(obj2)) / Math.PI);
+                    obj2.width = obj2.radius * 2;
+                    obj2.id.style.width = obj2.width + 'px';
+                    obj2.id.style.height = obj2.width + 'px';
+
+                    obj2.speedX = (obj.mass * obj.speedX + obj2.mass * obj2.speedX) / (obj.mass + obj2.mass);
+                    obj2.speedY = (obj.mass * obj.speedY + obj2.mass * obj2.speedY) / (obj.mass + obj2.mass);
+
+                    obj2.mass += obj.mass;
+
+                    document.body.removeChild(obj.id);
+                    dinamicObjects.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        obj.x += obj.speedX;
+        obj.y += obj.speedY;
+
+        obj.id.style.left = obj.x + 'px';
+        obj.id.style.top = obj.y + 'px';
+    }
+}
+
+// Calculate gravity force on the given object
 function addGravityAcceleration(obj) {
-    for (let i = 0; i < staticObjects.length; i++) {
-        var sObj = staticObjects[i];
+    for (let sObj of staticObjects) {
 
         let distances = getObjDistance(sObj, obj);
         let gForce = G * ((sObj.mass * obj.mass) / Math.pow(distances[0], 2));
@@ -44,8 +113,7 @@ function addGravityAcceleration(obj) {
         obj.speedY += (gForce / obj.mass) * cosB;
     }
 
-    for (let i = 0; i < dinamicObjects.length; i++) {
-        var obj2 = dinamicObjects[i];
+    for (let obj2 of dinamicObjects) {
 
         if (obj.id != obj2.id) {
             let distances = getObjDistance(obj, obj2);
@@ -72,16 +140,16 @@ function checkCollision(obj1, obj2) {
     return distances[0] + radius < obj1.radius + obj2.radius;
 }
 
+// Get distance between two objects
 function getObjDistance(obj1, obj2) {
     var xDistance = (obj1.x + obj1.radius) - (obj2.x + obj2.radius);
-    //console.log('X: ' + xDistance);
     var yDistance = (obj1.y + obj1.radius) - (obj2.y + obj2.radius);
-    //console.log('Y: ' + yDistance);
     var distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 
     return [distance, xDistance, yDistance];
 }
 
+// Get distance between two points
 function getTwoPointsDistance(x1, y1, x2, y2) {
     var xDistance = x1 - x2;
     var yDistance = y1 - y2;
@@ -90,57 +158,26 @@ function getTwoPointsDistance(x1, y1, x2, y2) {
     return [distance, xDistance, yDistance];
 }
 
-function moveObjects() {
-    for (let i = dinamicObjects.length - 1; i >= 0; i--) {
-        var obj = dinamicObjects[i];
-        var isObjRemoved = false;
-
-        addGravityAcceleration(obj);
-
-        for (let k = 0; k < staticObjects.length; k++) {
-            var sObj = staticObjects[k];
-            //console.log(checkCollision(sObj, obj));
-            if (checkCollision(sObj, obj) == true) {
-                document.body.removeChild(obj.id);
-                dinamicObjects.splice(i, 1);
-                isObjRemoved = true;
-                break;
-            }
-        }
-
-        for (let k = dinamicObjects.length - 1; k >= 0; k--) {
-            if (k != i && isObjRemoved == false) {
-                var obj2 = dinamicObjects[k];
-                if (checkCollision(obj, obj2) == true) {
-                    obj2.radius = Math.sqrt((getObjArea(obj) + getObjArea(obj2)) / Math.PI);
-                    obj2.width = obj2.radius * 2;
-                    obj2.id.style.width = obj2.width + 'px';
-                    obj2.id.style.height = obj2.width + 'px';
-
-                    obj2.speedX = (obj.mass * obj.speedX + obj2.mass * obj2.speedX) / (obj.mass + obj2.mass);
-                    obj2.speedY = (obj.mass * obj.speedY + obj2.mass * obj2.speedY) / (obj.mass + obj2.mass);
-
-                    obj2.mass += obj.mass;
-
-                    document.body.removeChild(obj.id);
-                    dinamicObjects.splice(i, 1);
-                    break;
-                }
-            }
-        }
-
-        obj.x += obj.speedX;
-        obj.y += obj.speedY;
-
-        obj.id.style.left = obj.x + 'px';
-        obj.id.style.top = obj.y + 'px';
-    }
-}
-
+// get area of a circle
 function getObjArea(obj) {
     return Math.PI * Math.pow(obj.radius, 2);
 }
 
+// Get the type of planet user has checked
+function getObjectType() {
+    var staticObjBtn = document.getElementById('staticObj');
+    var dinamicObjBtn = document.getElementById('dinamicObj');
+
+    if (staticObjBtn.checked) {
+        return 'static';
+    }
+
+    if (dinamicObjBtn.checked) {
+        return 'dinamic';
+    }
+}
+
+// Spawn a static or dinamic planet
 function spawnObject() {
     if (getObjectType() == 'static') {
         let object = {
@@ -180,7 +217,6 @@ function spawnObject() {
             x: 0,
             y: 0,
             width: 25,
-            //height: 25,
             radius: 25 / 2,
             mass: 5 * Math.pow(10, 11),
             speedX: 0,
@@ -196,7 +232,6 @@ function spawnObject() {
         object.id.style.width = object.width + 'px';
         object.id.style.height = object.width + 'px';
         object.id.style.backgroundColor = object.color;
-        //object.id.removeAttribute('draggable');
 
         object.x = clickCoordinates.x - object.radius;
         object.y = clickCoordinates.y - object.radius;
@@ -211,6 +246,7 @@ function spawnObject() {
     }
 }
 
+// Adds planet to the array of planets
 function addObjectToArr() {
     var obj = window.dinamicObj;
     addAcceleration(clickCoordinates, obj);
@@ -218,6 +254,7 @@ function addObjectToArr() {
     dinamicObjects.push(obj);
 }
 
+// Determines the speed of the object when mouse is dragged
 function addAcceleration(mousePos, obj) {
     var power = getTwoPointsDistance(mousePos.x, mousePos.y,
         mousePos.xUp, mousePos.yUp);
@@ -227,40 +264,38 @@ function addAcceleration(mousePos, obj) {
     obj.speedY = initialSpeed * power[2];
 }
 
-function getObjectType() {
-    var staticObjBtn = document.getElementById('staticObj');
-    var dinamicObjBtn = document.getElementById('dinamicObj');
-
-    if (staticObjBtn.checked == true) {
-        return 'static';
-    }
-
-    if (dinamicObjBtn.checked == true) {
-        return 'dinamic';
-    }
+// Draw a line when mouse is dragged
+function drawLine(x1, y1) {
+    newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    newLine.setAttribute('x1', x1);
+    newLine.setAttribute('y1', y1 - svg.getBoundingClientRect().top);
+    newLine.setAttribute('x2', x1);
+    newLine.setAttribute('y2', y1 - svg.getBoundingClientRect().top);
+    newLine.setAttribute("stroke", "green");
+    newLine.setAttribute("stroke-width", "2");
+    svg.append(newLine);
 }
 
+// When check box is checked the other one is unchecked
 function changeInput(type) {
     var staticObjBtn = document.getElementById('staticObj');
     var dinamicObjBtn = document.getElementById('dinamicObj');
 
-    if (type == 1 && staticObjBtn.checked == false) {
+    if (type == 1 && !staticObjBtn.checked) {
         dinamicObjBtn.checked = true;
-    } else if (type == 1 && staticObjBtn.checked == true) {
+    } else if (type == 1 && staticObjBtn.checked) {
         dinamicObjBtn.checked = false;
     }
 
-    if (type == 2 && dinamicObjBtn.checked == false) {
+    if (type == 2 && !dinamicObjBtn.checked) {
         staticObjBtn.checked = true;
-    } else if (type == 2 && staticObjBtn.checked == true) {
+    } else if (type == 2 && staticObjBtn.checked) {
         staticObjBtn.checked = false;
     }
 }
 
+// Get X and Y coordinates of mouse when is pressed
 function getMousePos1(event) {
-    //console.log(event.clientX);
-    //console.log(event.clientY);
-
     clickCoordinates.x = event.clientX;
     clickCoordinates.y = event.clientY;
 
@@ -269,24 +304,38 @@ function getMousePos1(event) {
     } else {
         spawnObject();
     }
+
+    if (document.getElementById('dinamicObj').checked) {
+        drawLine(clickCoordinates.x, clickCoordinates.y);
+    }
+    isMouseDown = true;
 }
 
+// Get X and Y coordinates of mouse when is released
 function getMousePos2(event) {
-    //console.log(event.clientX);
-    //console.log(event.clientY);
 
     clickCoordinates.xUp = event.clientX;
     clickCoordinates.yUp = event.clientY;
 
-    //console.log(clickCoordinates);
-    /*console.log(getTwoPointsDistance(clickCoordinates.x, clickCoordinates.y,
-        clickCoordinates.xUp, clickCoordinates.yUp))*/
-
-    if (isObjectSpawned == true) {
+    if (isObjectSpawned) {
         addObjectToArr();
         isObjectSpawned = false;
     }
+
+    isMouseDown = false;
+    svg.removeChild(newLine);
+}
+
+// Get X and Y coordinates of mouse when is dragged
+function getMouseDownPos(event) {
+
+    clickCoordinates.xMove = event.clientX;
+    clickCoordinates.yMove = event.clientY - svg.getBoundingClientRect().top;
 }
 
 document.addEventListener("mousedown", getMousePos1);
 document.addEventListener("mouseup", getMousePos2);
+
+document.onmousemove = function(event) {
+    getMouseDownPos(event);
+}
